e004ed338943cd3b6a753b83ac3806ba209a7bd8
william <william@adamantio.com.br>
2016-04-18 19:13:01 -0300

    criacao do repositorio

---
+++ ada-loading-bar.js
@@ -0,0 +1,61 @@
+loadingBar = angular.module('ada-loading-bar',[]);
+loadingBar.factory('LoadingBar',function($q){
+
+	var running = false;
+
+	return{
+
+		/**
+		 * Shows the loading bar
+		 */
+		show : function(){
+			if (!running){
+				running = true;
+				$("#loading").modal('show');
+			}
+		},
+
+		/**
+		 * Hides the loading bar
+		 */
+		hide : function(){
+			var deferred = $q.defer();
+			if (running){
+				running = false;
+				$("#loading").modal('hide');
+				$("#loading").on('hidden.bs.modal',function(){
+					deferred.resolve();
+				});
+			}
+
+			return deferred.promise;
+		},
+
+		/**
+		 * Toggles the loading bar
+		 */ 
+		toggle : function(){
+			running = !running;
+			$("#loading").modal('toggle');
+		}
+	};
+	
+}).directive('loading', function(){
+
+	var script = document.querySelector("[src$='loading-bar.js']");	
+
+	if (!script) console.error("loading-bar.js file not found");
+
+	var currentScriptPath = script.src;
+
+	console.error();
+
+	return {
+		restrict: 'E',
+		scope:{
+			msg:'@message',
+			icon:'=icon',
+		},
+		templateUrl:currentScriptPath.replace('loading-bar.js', 'loading-bar.html')
+	};
+});
